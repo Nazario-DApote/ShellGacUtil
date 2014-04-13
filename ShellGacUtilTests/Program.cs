97b51bcf976a73db7d8f6ef2d28807646e0df271
Nazario D'Apote <nazario.dapote@gmail.com>
2014-04-13 15:55:03 +0200

    Added ShellGacUtilTests project

---
+++ ShellGacUtilTests/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using ShellGacUtil;
+
+namespace ShellGacUtilTests
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            var msgDlg = new MessageDialog();
+            msgDlg.EnableClipboardCopy = true;
+            msgDlg.QualifiedName = "QualifiedName";
+            msgDlg.MessageText = "MessageText";
+            msgDlg.MessageDetails = "MessageDetails";
+
+            Application.Run(msgDlg);
+        }
+    }
+}
